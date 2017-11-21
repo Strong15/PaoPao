33488d1855443c38a7ec05a221c681d8f227584a
wangkui <769469309@qq.com>
2017-11-21 21:42:38 +0800

    第一次提交

---
+++ src/main/java/com/itheima/springboottest/SpringboottestApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.springboottest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+
+	@Value("${cupSize}")
+	private String cupSize;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestApplication.class, args);
+		System.out.println(123);
+	}
+}
