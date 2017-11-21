c1361bac75ea3187ac8802c44036122139d4f172
wangkui <769469309@qq.com>
2017-11-21 21:53:23 +0800

    第二次提交

--- src/main/java/com/itheima/springboottest/SpringboottestApplication.java
+++ src/main/java/com/itheima/springboottest/SpringboottestApplication.java
@@ -12,5 +12,6 @@ public class SpringboottestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestApplication.class, args);
-	}
+        System.out.println("你好");
+    }
 }
