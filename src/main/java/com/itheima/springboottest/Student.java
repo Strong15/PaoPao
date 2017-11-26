acf26019435725490ae4f5dc2eedfeb9ef928d31
wangkui <769469309@qq.com>
2017-11-26 16:50:53 +0800

    添加一个类

---
+++ src/main/java/com/itheima/springboottest/Student.java
@@ -0,0 +1,13 @@
+package com.itheima.springboottest;
+
+public class Student {
+     private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
